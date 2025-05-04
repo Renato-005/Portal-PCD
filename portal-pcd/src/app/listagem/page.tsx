b211c5d1b594bd378435a0dddf5cd831a9618397
Renato-005 <rbezerra979@gmail.com>
2025-05-04 20:22:04 -0300

    adicionando tailwind css

--- portal-pcd/src/app/listagem/page.tsx
+++ portal-pcd/src/app/listagem/page.tsx
@@ -17,30 +17,30 @@ export default async function ListagemPage() {
         <h1>Bem Vindo ao DashBoard</h1>
         <p>Esta página é protegida por Login!</p>
         
-        <div>
+        <div className="max-w-xl mx-auto mt-10 p-6 bg-white rounded-2xl shadow-md">
           <a href="https://www.gov.br/saude/pt-br" target="_blank" rel="noopener noreferrer">
-            <p>Serviços de Saúde e Reabilitação</p>
+            <p className="text-center text-lg mb-3 rounded-lg hover:bg-blue-100 transition-colors cursor-pointer">Serviços de Saúde e Reabilitação</p>
           </a>
           <a href="https://www.gov.br/inss/pt-br/assuntos/beneficios" target="_blank" rel="noopener noreferrer">
-            <p>Benefícios e Direitos</p>
+            <p className="text-center text-lg mb-3 rounded-lg hover:bg-blue-100 transition-colors cursor-pointer">Benefícios e Direitos</p>
           </a>
           <a href="https://empregabrasil.mte.gov.br/" target="_blank" rel="noopener noreferrer">
-            <p>Emprego e Profissionalização</p>
+            <p className="text-center text-lg mb-3 rounded-lg hover:bg-blue-100 transition-colors cursor-pointer">Emprego e Profissionalização</p>
           </a>
           <a href="https://www.gov.br/mec/pt-br" target="_blank" rel="noopener noreferrer">
-            <p>Educação Inclusiva</p>
+            <p className="text-center text-lg mb-3 rounded-lg hover:bg-blue-100 transition-colors cursor-pointer">Educação Inclusiva</p>
           </a>
           <a href="https://www.ctpsdigital.gov.br/acessibilidade" target="_blank" rel="noopener noreferrer">
-            <p>Acessibilidade Urbana e Mobilidade</p>
+            <p className="text-center text-lg mb-3 rounded-lg hover:bg-blue-100 transition-colors cursor-pointer">Acessibilidade Urbana e Mobilidade</p>
           </a>
           <a href="https://www.cvv.org.br/" target="_blank" rel="noopener noreferrer">
-            <p>Comunidades e Apoio Psicológico</p>
+            <p className="text-center text-lg mb-3 rounded-lg hover:bg-blue-100 transition-colors cursor-pointer">Comunidades e Apoio Psicológico</p>
           </a>
           <a href="https://assistiva.org.br/" target="_blank" rel="noopener noreferrer">
-            <p>Produtos e Tecnologia Assistiva</p>
+            <p className="text-center text-lg mb-3 rounded-lg hover:bg-blue-100 transition-colors cursor-pointer">Produtos e Tecnologia Assistiva</p>
           </a>
           <a href="https://www.gov.br/mdh/pt-br/ondh/denuncias" target="_blank" rel="noopener noreferrer">
-            <p>Denúncias e Reclamações</p>
+            <p className="text-center text-lg mb-3 rounded-lg hover:bg-blue-100 transition-colors cursor-pointer">Denúncias e Reclamações</p>
           </a>
       </div>
         
