a753ad8337368162fb1683aca526541339d6de80
Renato-005 <rbezerra979@gmail.com>
2025-05-04 21:39:40 -0300

    arrumando menu

--- portal-pcd/src/components/Menu/Menu.tsx
+++ portal-pcd/src/components/Menu/Menu.tsx
@@ -5,26 +5,18 @@ export default function Menu() {
         <nav className="mt-4">
           <ul className="flex gap-6 justify-center">
             <li>
-              <Link href="/cadastro" className="text-gray-700 hover:text-gray-900 font-medium">
-                Cadastrar
-              </Link>
+              <Link href="/cadastro" className="text-gray-700 hover:text-gray-900 font-medium">Cadastrar</Link>
             </li>
             <li>
-              <Link href="/login" className="text-gray-700 hover:text-gray-900 font-medium">
-                Login
-              </Link>
+              <Link href="/login" className="text-gray-700 hover:text-gray-900 font-medium">Login</Link>
             </li>
             <li>
-              <Link href="/listagem" className="text-gray-700 hover:text-gray-900 font-medium">
-                Listagem
-              </Link>
+              <Link href="/listagem" className="text-gray-700 hover:text-gray-900 font-medium">Listagem</Link>
             </li>
             <li>
-              <Link href="/lista" className="text-gray-700 hover:text-gray-900 font-medium">
-                Listar
-              </Link>
+              <Link href="/lista" className="text-gray-700 hover:text-gray-900 font-medium">Listar</Link>
             </li>
           </ul>
         </nav>
-      );
-    }
+    );
+}
